88ea5990202c53095186f9cdc212cfbe7c60620a
syrine <syrine.radhouane@etu.unice.fr>
2021-02-27 18:57:14 +0100

    Clean App component and bring out Main component

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,13 @@
-import { Route, useRouteMatch, useLocation } from "react-router-dom";
+import { useRouteMatch, useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useMediaPredicate } from "react-media-hook";
-import { Layout, Switch as SwitchBtn, Tabs, Spin, Divider } from "antd";
-import { Row, Col } from "antd";
+import { Layout, Switch as SwitchBtn } from "antd";
 
-// import "antd/dist/antd.dark.css";
-import "antd/dist/antd.compact.css";
-
-import { NationalSituation } from "./components/NationalSituation.js";
-import { Filters } from "./components/Filters.js";
-import { Map } from "./components/Map.js";
-import { DataTable } from "./components/DataTable.js";
-import { Chart } from "./components/Chart.js";
-
+import { Main } from "./components/Main.js";
 import { fetchFromBackend } from "./lib/fetchFromBackend";
 
-const { Header, Content, Footer } = Layout;
-const { TabPane } = Tabs;
+const { Header, Footer } = Layout;
 
-/**
- * comment
- */
 function App() {
     const [tabData, setTabData] = useState([]);
     const [isTabDataLoaded, setIsTabDataLoaded] = useState(false);
@@ -78,45 +65,12 @@ function App() {
                     />
                 </div>
             </Header>
-            <Content
-                style={{
-                    margin: "24px 50px",
-                    padding: 24,
-                    minHeight: 280,
-                    background: "#fff",
-                }}
-            >
-                <Route path="/">
-                    <Filters />
-                    <NationalSituation />
-                    <Route path={["/reg", "/dep"]}>
-                        <Map />
-                    </Route>
-                    <Spin spinning={!isTabDataLoaded} tip="Chargement...">
-                        <Divider>
-                            <h1 style={{ fontSize: "60px" }}>
-                                {selectedRegion
-                                    ? selectedRegion.name
-                                    : selectedDepartement
-                                    ? selectedDepartement.name
-                                    : "France"}
-                            </h1>
-                        </Divider>
-                        <Tabs defaultActiveKey="1">
-                            <TabPane tab="Taux d'incidence standardisé" key="1">
-                                <Row gutter={16}>
-                                    <Col md={24} lg={11}>
-                                        <DataTable data={tabData} />
-                                    </Col>
-                                    <Col md={24} lg={13}>
-                                        <Chart data={tabData} />
-                                    </Col>
-                                </Row>
-                            </TabPane>
-                        </Tabs>
-                    </Spin>
-                </Route>
-            </Content>
+            <Main
+                isTabDataLoaded={isTabDataLoaded}
+                selectedRegion={selectedRegion}
+                selectedDepartement={selectedDepartement}
+                tabData={tabData}
+            />
             <Footer style={{ textAlign: "center" }}></Footer>
         </Layout>
     );
---
+++ client/src/components/Main.js
@@ -0,0 +1,61 @@
+import { Route } from "react-router-dom";
+import { Layout, Tabs, Spin, Divider } from "antd";
+import { Row, Col } from "antd";
+
+import { NationalSituation } from "./NationalSituation.js";
+import { Filters } from "./Filters.js";
+import { Map } from "./Map.js";
+import { DataTable } from "./DataTable.js";
+import { Chart } from "./Chart.js";
+
+const { Content } = Layout;
+const { TabPane } = Tabs;
+
+export const Main = ({
+    isTabDataLoaded,
+    selectedRegion,
+    selectedDepartement,
+    tabData,
+}) => {
+    return (
+        <Content
+            style={{
+                margin: "24px 50px",
+                padding: 24,
+                minHeight: 280,
+                background: "#fff",
+            }}
+        >
+            <Route path="/">
+                <Filters />
+                <NationalSituation />
+                <Route path={["/reg", "/dep"]}>
+                    <Map />
+                </Route>
+                <Spin spinning={!isTabDataLoaded} tip="Chargement...">
+                    <Divider>
+                        <h1 style={{ fontSize: "60px" }}>
+                            {selectedRegion
+                                ? selectedRegion.name
+                                : selectedDepartement
+                                ? selectedDepartement.name
+                                : "France"}
+                        </h1>
+                    </Divider>
+                    <Tabs defaultActiveKey="1">
+                        <TabPane tab="Taux d'incidence standardisé" key="1">
+                            <Row gutter={16}>
+                                <Col md={24} lg={11}>
+                                    <DataTable data={tabData} />
+                                </Col>
+                                <Col md={24} lg={13}>
+                                    <Chart data={tabData} />
+                                </Col>
+                            </Row>
+                        </TabPane>
+                    </Tabs>
+                </Spin>
+            </Route>
+        </Content>
+    );
+};
